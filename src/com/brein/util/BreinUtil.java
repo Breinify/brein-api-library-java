b1fb18c709327d7e3625bbd840c04f8516c69789
Marco <marco.recchioni@inform-ac.com>
2016-06-26 14:36:57 +0200

    containsValue method added

---
+++ src/com/brein/util/BreinUtil.java
@@ -0,0 +1,32 @@
+package com.brein.util;
+
+/**
+ *
+ */
+public class BreinUtil {
+
+    /**
+     * Verifies if the object contains a value.
+     * Return false in case of:
+     *    - null
+     *    - empty strings
+     *
+     * @param object to check
+     *
+     * @return true if object contains data
+     */
+    public static boolean containsValue(final Object object) {
+
+        if (object == null) {
+            return false;
+        }
+
+        if (object.getClass() == String.class) {
+            final String strObj = (String)object;
+            return strObj.length() > 0;
+        }
+
+        return true;
+    }
+
+}
