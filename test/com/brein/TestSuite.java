d6f83c90345bddc7807e99d3788141f1fd9e234f
Marco <marco.recchioni@inform-ac.com>
2016-10-08 19:27:10 +0200

    TestUtil added

--- test/com/brein/TestSuite.java
+++ test/com/brein/TestSuite.java
@@ -2,6 +2,7 @@ package com.brein;
 
 import com.brein.config.TestConfig;
 import com.brein.domain.TestDomain;
+import com.brein.util.TestUtil;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
@@ -11,7 +12,8 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         TestConfig.class,
-        TestDomain.class
+        TestDomain.class,
+        TestUtil.class
 })
 public class TestSuite {
 }
