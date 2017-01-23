a0250b3835fd539632fa96c40d80c6d7a51dd877
Marco <marco.recchioni@inform-ac.com>
2017-01-24 00:22:21 +0100

    simplification of Jenkinsfile usage

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,9 +1,12 @@
 node('master') {
 
     def master = load '../brein-workspace/brein-intellij-workspace/common-libs/jenkins/jenkins.master.groovy'
+
+    master.checkOutBuildTestAndDeploy('brein-api-library/brein-api-library-java')
+    
+    /*
     master.checkOutBreinRepo()
-
     master.buildAndTest('brein-api-library/brein-api-library-java')
-
     master.deploy('brein-api-library/brein-api-library-java')
+    */
 }
