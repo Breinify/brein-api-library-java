ae428ce18564acd429f217f30b3eb27f1371259e
Marco <marco.recchioni@inform-ac.com>
2017-01-24 00:17:58 +0100

    usage of Jenkins master file

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,44 +1,9 @@
-
-// #!/usr/bin/groovy
-
-
-
 node('master') {
 
     def master = load '../brein-workspace/brein-intellij-workspace/common-libs/jenkins/jenkins.master.groovy'
     master.checkOutBreinRepo()
-    master.mailTest()
 
-    // it is enough to run the test, it will resolve, build and test
-    stage ('Test & Build') {
-        try {
-            dir ('brein-api-library/brein-api-library-java') {
-                sh 'ant 06-run-test-suite'
-            }
-        } catch (err) {
+    master.buildAndTest('brein-api-library/brein-api-library-java')
 
-            emailext (
-                    to: 'marco@breinify.com',
-                    subject: "${env.JOB_NAME} #${env.BUILD_NUMBER} [${currentBuild.result}]",
-                    body: "Build URL: ${env.BUILD_URL} failed.\n\n",
-                    attachLog: true,
-            )
-
-            // emailext body: 'Failure in step: Test & Build', recipientProviders: [[$class: 'CulpritsRecipientProvider']], subject: 'Jenkins Error'
-            throw err
-        }
-    }
-
-    // now we should just publish the new version, it made it through the test
-    stage ('Deploy') {
-        try {
-            dir ('brein-api-library/brein-api-library-java') {
-                sh 'ant 04-publish-results'
-            }
-        } catch (err) {
-            emailext body: 'Failure in step: Deploy', recipientProviders: [[$class: 'CulpritsRecipientProvider']], subject: 'Jenkins Error'
-            emailextrecipients([[$class: 'CulpritsRecipientProvider']])
-            throw err
-        }
-    }
+    master.deploy('brein-api-library/brein-api-library-java')
 }
