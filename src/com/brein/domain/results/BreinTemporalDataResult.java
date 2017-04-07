c29550be9218fd62523f9fb0ad3cf934dbe070d7
Todd Bodnar <todd.bodnar@breinify.com>
2017-04-07 11:29:04 -0700

    local/epoch time

--- src/com/brein/domain/results/BreinTemporalDataResult.java
+++ src/com/brein/domain/results/BreinTemporalDataResult.java
@@ -3,11 +3,16 @@ package com.brein.domain.results;
 import com.brein.domain.BreinResult;
 import com.brein.domain.results.temporaldataparts.BreinWeatherResult;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.Map;
 
 public class BreinTemporalDataResult extends BreinResult {
     private static final String WEATHER_KEY = "weather";
+    private static final String TIME_KEY = "time";
+    private static final String LOCAL_TIME_KEY = "localFormatIso8601";
+    private static final String EPOCH_TIME_KEY = "epochFormatIso8601";
 
     public BreinTemporalDataResult(final Map<String, Object> json) {
         super(json == null ? Collections.emptyMap() : json);
@@ -24,6 +29,30 @@ public class BreinTemporalDataResult extends BreinResult {
     public BreinWeatherResult getWeather() {
         //noinspection unchecked
         return new BreinWeatherResult((Map<String, Object>) getMap().get(WEATHER_KEY));
+    }
+
+    public boolean hasLocalDateTime() {
+        //noinspection unchecked
+        return getMap().containsKey(TIME_KEY) &&
+                ((Map<String, Object>) getMap().get(TIME_KEY)).containsKey(LOCAL_TIME_KEY);
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        //noinspection unchecked
+        return LocalDateTime.parse(((Map<String, Object>) getMap().get(TIME_KEY)).get(LOCAL_TIME_KEY).toString(),
+                DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+    }
+
+    public boolean hasEpochDateTime() {
+        //noinspection unchecked
+        return getMap().containsKey(TIME_KEY) &&
+                ((Map<String, Object>) getMap().get(TIME_KEY)).containsKey(EPOCH_TIME_KEY);
+    }
+
+    public LocalDateTime getEpochDateTime() {
+        //noinspection unchecked
+        return LocalDateTime.parse(((Map<String, Object>) getMap().get(TIME_KEY)).get(EPOCH_TIME_KEY).toString(),
+                DateTimeFormatter.ISO_OFFSET_DATE_TIME);
     }
 
     @Override
--- test/com/brein/domain/results/TestBreinTemporalDataResult.java
+++ test/com/brein/domain/results/TestBreinTemporalDataResult.java
@@ -5,6 +5,8 @@ import com.brein.domain.results.temporaldataparts.PrecipitationType;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
 import java.util.Collections;
 
 public class TestBreinTemporalDataResult {
@@ -25,5 +27,45 @@ public class TestBreinTemporalDataResult {
         Assert.assertTrue(withWeather.hasWeather());
         Assert.assertEquals(PrecipitationType.RAIN, withWeather.getWeather().getPrecipitation());
         Assert.assertEquals(1234.0, withWeather.getWeather().getPrecipitationAmount(), 0.001);
+    }
+
+    @Test
+    public void testLocalTime() {
+        final String json = "{ \"time\": {\"localFormatIso8601\": \"2017-04-07T12:02:46-05:00\"}}";
+
+        Assert.assertFalse(new BreinTemporalDataResult(Collections.emptyMap()).hasLocalDateTime());
+
+        final BreinTemporalDataResult withTime = new BreinTemporalDataResult(new BreinResult(json));
+        Assert.assertTrue(withTime.hasLocalDateTime());
+        final LocalDateTime time = withTime.getLocalDateTime();
+
+        Assert.assertEquals(2017, time.getYear());
+        Assert.assertEquals(4, time.getMonthValue());
+        Assert.assertEquals(7, time.getDayOfMonth());
+        Assert.assertEquals(DayOfWeek.FRIDAY, time.getDayOfWeek());
+
+        Assert.assertEquals(12, time.getHour());
+        Assert.assertEquals(2, time.getMinute());
+        Assert.assertEquals(46, time.getSecond());
+    }
+
+    @Test
+    public void testEpochTime() {
+        final String json = "{ \"time\": {\"epochFormatIso8601\": \"2017-04-07T17:02:46+00:00\"}}";
+
+        Assert.assertFalse(new BreinTemporalDataResult(Collections.emptyMap()).hasEpochDateTime());
+
+        final BreinTemporalDataResult withTime = new BreinTemporalDataResult(new BreinResult(json));
+        Assert.assertTrue(withTime.hasEpochDateTime());
+        final LocalDateTime time = withTime.getEpochDateTime();
+
+        Assert.assertEquals(2017, time.getYear());
+        Assert.assertEquals(4, time.getMonthValue());
+        Assert.assertEquals(7, time.getDayOfMonth());
+        Assert.assertEquals(DayOfWeek.FRIDAY, time.getDayOfWeek());
+
+        Assert.assertEquals(17, time.getHour());
+        Assert.assertEquals(2, time.getMinute());
+        Assert.assertEquals(46, time.getSecond());
     }
 }
