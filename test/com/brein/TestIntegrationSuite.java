2a5f34b950a9071942593db80574bf19dc12d554
Philipp Meisen <philipp@meisen.net>
2017-04-18 14:47:51 -0700

    documentation

--- test/com/brein/TestIntegrationSuite.java
+++ test/com/brein/TestIntegrationSuite.java
@@ -1,14 +1,13 @@
 package com.brein;
 
+import com.brein.api.TestActivityApiWithJersey;
 import com.brein.api.TestActivityApiWithSecret;
 import com.brein.api.TestActivityApiWithUnirest;
-import com.brein.api.TestActivityApiWithJersey;
 import com.brein.api.TestConcurrencyWithJersey;
 import com.brein.api.TestConcurrencyWithSecret;
 import com.brein.api.TestConcurrencyWithUniRest;
 import com.brein.api.TestLookupApi;
 import com.brein.api.TestStressTests;
-import com.brein.api.TestTemporalDataApiWithUnirest;
 import com.brein.engine.TestEngine;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -18,7 +17,6 @@ import org.junit.runners.Suite;
         TestActivityApiWithUnirest.class,
         TestActivityApiWithJersey.class,
         TestActivityApiWithSecret.class,
-        //TestTemporalDataApiWithUnirest.class,
         TestLookupApi.class,
         TestEngine.class,
         TestStressTests.class,
